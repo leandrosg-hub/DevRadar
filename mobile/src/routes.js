0d96ca0b8b4a88e40a1203751cd2d1344e5d146f
leandrosg-hub <leandrosg69@gmail.com>
2020-05-14 16:29:32 -0300

    atualizado

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,32 +1,19 @@
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import Main from './pages/Main'
 import Profile from './pages/Profile'
 
-const Routes = createAppContainer(
-    createStackNavigator({
-        Main: {
-            screen: Main,
-            navigationOptions: {
-                title: 'DevRadar'
-            }
-        },
-        Profile: {
-            screen: Profile,
-            navigationOptions: {
-                title: 'Perfil no Github'
-            }
-        }
-    }, {
-        defaultNavigationOptions: {
-            headerTintColor: '#FFF',
-            headerBackTitleVisible: false,
-            headerStyle: {
-                backgroundColor: '#7d40e7'
-            }
-        }
-    })
-)
+const Stack = createStackNavigator()
 
-export default Routes
+export default function Routes() {
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Main" component={Main} />
+                <Stack.Screen name="Profile" component={Profile} />
+            </Stack.Navigator>            
+        </NavigationContainer>
+    ) 
+}
